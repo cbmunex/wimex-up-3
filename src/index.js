1ed98e0093d36cfd29b207f698d4dc87453cebaa
cbmunex <cbmunex@gmail.com>
2025-12-04 16:21:50 -0300

    feat: nova home estilo landing page e logo Wimex-UP

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+//import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
 
